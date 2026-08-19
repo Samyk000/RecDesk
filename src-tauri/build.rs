1882b76a5ff08a7a09e45daf61301446105c9f84
Samyk000 <sameer.amor00@gmail.com>
2026-08-19 06:56:11 +0530

    Add build.rs icon change triggers and bundle icons list

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,7 @@
 fn main() {
-    tauri_build::build()
+    println!("cargo:rerun-if-changed=icons/icon.ico");
+    println!("cargo:rerun-if-changed=icons/32x32.png");
+    println!("cargo:rerun-if-changed=icons/128x128.png");
+    println!("cargo:rerun-if-changed=tauri.conf.json");
+    tauri_build::build();
 }
